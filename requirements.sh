faf2820e6ce2dde5577321213774c1bfa7080e23
Francis Di-Folco <fdifolco@dept-info.crosemont.quebec>
2024-02-20 16:12:06 -0500

    update

--- requirements.sh
+++ requirements.sh
@@ -5,7 +5,7 @@ sudo dpkg -i cuda-repo-wsl-ubuntu-11-8-local_11.8.0-1_amd64.deb
 sudo cp /var/cuda-repo-wsl-ubuntu-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get -y install cuda
-apt install -y python3.11
-apt install -y python3-pip
-apt install -y python3-venv
-apt install -y nvidia-cuda-toolkit
+sudo apt install -y python3.11
+sudo apt install -y python3-pip
+sudo apt install -y python3-venv
+sudo apt install -y nvidia-cuda-toolkit
